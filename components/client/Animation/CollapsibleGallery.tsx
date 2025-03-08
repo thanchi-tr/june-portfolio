91cfda22a4f3fd552b928f4d6811b47ed09d30a4
June <june-tr@outlook.com>
2025-03-09 01:15:09 +1100

    update

--- components/client/Animation/CollapsibleGallery.tsx
+++ components/client/Animation/CollapsibleGallery.tsx
@@ -15,43 +15,78 @@ const CollapsibleGallery = ({ cards }: CollapsibleGalleryConfig) => {
         `}>
             {cards.map(
                 (card, index) => (
-
-                    <motion.div
-                        key={`cGallery-${index}`}
-                        style={{ width: ((index != selectedIndex) ? "4%" : `${Math.floor(97 - (cards.length - 1) * 4)}%`) }}
-                        className={`
-                            inline overflow-clip
+                    <>
+                        <div
+                            key={`cGallery-${index}`}
+                            style={{ width: ((index != selectedIndex) ? "4%" : `${Math.floor(97 - (cards.length - 1) * 4)}%`) }}
+                            className={`
+                            inline overflow-clip lg:hidden
                             h-full relative will-change-[transform,opacity]
                             ${index == selectedIndex ? "shadow-xl shadow-black rounded-xl border-[1px] border-b-white/15" : "rounded-md"}
                             ${index == selectedIndex ? "bg-transparent pointer-events-none" : "bg-gradient-to-t from-primary via-primary/50 border-t-[1.2vw] border-primary border-opacity-35 to-black/30 hover:cursor-pointer "}
                             transition-all duration-[1.5s] ease-out
                             `}
-                        onClick={() => setSelectedIndex(index)}
-                        initial={selectedIndex != index ? { translateY: "20%" } : { scaleX: "40%" }}
-                        transition={{ duration: 0.5, delay: 0.2 * index }}
-                    >
+                            onClick={() => setSelectedIndex(index)}
+                        >
 
-                        {
-                            <div
-                                className={`relative origin-top-right h-full w-full will-change-[transform,opacity]
+                            {
+                                <div
+                                    className={`relative origin-top-right h-full w-full will-change-[transform,opacity]
                                     transition-all   ease-in-out ${index != selectedIndex ? "duration-75 opacity-0" : "duration-[390ms] delay-1000 opacity-100"}
                                 `}
-
-                            >
-                                {card}
-                                {/* <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-[#ff8c00] to-[#8a2be2] opacity-20 blur-3xl scale-y-75"></div> */}
-                            </div>
-                        }
-                        <div className={`
+                                >
+                                    {card}
+                                    {/* <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-[#ff8c00] to-[#8a2be2] opacity-20 blur-3xl scale-y-75"></div> */}
+                                </div>
+                            }
+                            <div className={`
                             h-full w-full 
                             transition-all duration-[0.32s] ease-in-out
                             ${index == selectedIndex ? "rounded-xl" : "rounded-md backdrop-blur-2xl hover:bg-background"}
                             z-[100] `} />
-                        {/* The index display*/}
+                            {/* The index display*/}
 
-                    </motion.div>
+                        </div>
+                        <motion.div
+                            key={`cGallery2-${index}`}
+                            style={{ width: ((index != selectedIndex) ? "4%" : `${Math.floor(97 - (cards.length - 1) * 4)}%`) }}
+                            className={`
+                             overflow-clip hidden lg:inline
+                            h-full relative will-change-[transform,opacity]
+                            ${index == selectedIndex ? "shadow-xl shadow-black rounded-xl border-[1px] border-b-white/15" : "rounded-md"}
+                            ${index == selectedIndex ? "bg-transparent pointer-events-none" : "bg-gradient-to-t from-primary via-primary/50 border-t-[1.2vw] border-primary border-opacity-35 to-black/30 hover:cursor-pointer "}
+                            transition-all duration-[1.5s] ease-out
+                            `}
+                            onClick={() => setSelectedIndex(index)}
+                            initial={selectedIndex != index ? { translateY: "20%" } : { scaleX: "40%" }}
+                            whileInView={selectedIndex != index ? { translateY: ["20%", "0%"], opacity: [0, 1] } : { scaleX: ["40%", "100%"] }}
+                            transition={{ duration: 0.5, delay: 0.2 * index }}
 
+                            viewport={{ once: true }}
+                        >
 
+                            {
+                                <motion.div
+                                    className={`relative origin-top-right h-full w-full will-change-[transform,opacity]
+                                    transition-all   ease-in-out ${index != selectedIndex ? "duration-75 opacity-0" : "duration-[390ms] delay-1000 opacity-100"}
+                                `}
+                                    whileInView={selectedIndex == index ? { opacity: [0, 1] } : { opacity: 0 }}
+                                    viewport={{ once: true }}
+                                    transition={selectedIndex == index ? { duration: 0.6, delay: 0.2 * index + 1 } : { duration: 0.1 }}
+                                >
+                                    {card}
+                                    {/* <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-[#ff8c00] to-[#8a2be2] opacity-20 blur-3xl scale-y-75"></div> */}
+                                </motion.div>
+                            }
+                            <div className={`
+                            h-full w-full 
+                            transition-all duration-[0.32s] ease-in-out
+                            ${index == selectedIndex ? "rounded-xl" : "rounded-md backdrop-blur-2xl hover:bg-background"}
+                            z-[100] `} />
+                            {/* The index display*/}
+
+                        </motion.div>
+                    </>
 
                 )
             )}
