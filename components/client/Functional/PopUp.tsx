baa98f56a62c0d45d637620db5579520df71e5c4
June <june-tr@outlook.com>
2025-03-08 12:53:10 +1100

    add response from tap

--- components/client/Functional/PopUp.tsx
+++ components/client/Functional/PopUp.tsx
@@ -1,34 +1,37 @@
 'use client';
 
-import { useState, ReactNode } from "react";
+import { ReactNode } from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
 interface PopUpProps {
     children: ReactNode,
     img: string
 }
 const PopUp = ({ children, img }: PopUpProps) => {
-    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <div className={`
                 relative h-full w-full 
                 group cursor-pointer bg-transparent
         `}
-            onClick={() => setIsOpen(prev => !prev)}
+
         >
-            <div className="relative h-auto w-full aspect-square overflow-clip rounded-full">
+            <motion.div className="relative h-auto w-full aspect-square overflow-clip rounded-full"
+                whileTap={{ scale: 0.9 }}
+            >
                 <Image
                     src={img}
                     alt={"Clickable Image"} fill
                     className="object-contain aspect-square"
                     quality={50}
                 />
-            </div>
+            </motion.div>
             <div className={`
                 absolute top-0 flex flex-wrap 
                 h-auto w-full -translate-y-[90%] 
                 text-sm md:text-md font-bold text-center
                 group-hover:opacity-100
-                ${isOpen ? "opacity-100" : "opacity-0"}
+                opacity-0
             `}
 
             >
