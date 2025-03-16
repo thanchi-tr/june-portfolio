9e85bb471aff60713f0c5608ed8e01b662fb000b
June <june-tr@outlook.com>
2025-03-17 01:32:54 +1100

    adjust the form stylr to suit the whole website vibe

--- components/client/Functional/Form.tsx
+++ components/client/Functional/Form.tsx
@@ -44,26 +44,26 @@ const ContactForm = () => {
 
     return (
         <div className="max-w-sm w-[80%] mx-auto p-2 bg-transparent border-t-2 border-x-2 border-x-white/20 border-t-white/50 border-b-4 border-b-gray-900 shadow-2xl shadow-violet-700/90 rounded-lg -translate-y-6 z-50 ">
-            <h2 className="text-2xl font-bold mb-4 lg:opacity-0 uppercase"> Contact me</h2>
+            <h2 className="text-2xl font-bold mb-4 lg:opacity-0 text-gray-500"><p className='text-white inline-block uppercase pt-4 font-extrabold gradient-text'> Contact</p> me</h2>
             {success && <p className="text-green-900/50">Message sent successfully!</p>}
             {error && <p className="text-red-900/80">{error}</p>}
             <form onSubmit={handleSubmit(onSubmit)} className="space-y-3 z-50">
                 <div>
                     <label className="block font-medium text-gray-300">Your Name</label>
-                    <input {...register('name')} className="w-full border-2 p-1 bg-white rounded" />
+                    <input {...register('name')} className="w-full border-2 p-1 bg-white/90 rounded-xl" />
                     {errors.name && <p className="text-red-900/80 text-xs">{errors.name.message}</p>}
                 </div>
                 <div>
                     <label className="block font-medium text-gray-300">How can I contact you ?</label>
-                    <input {...register('email')} placeholder="Your Email" className="w-full border-2 p-1 bg-white   rounded" />
+                    <input {...register('email')} placeholder="Your Email" className="w-full border-2 p-1 bg-white/90   rounded-xl" />
                     {errors.email && <p className="text-red-900/80 text-xs ">{errors.email.message}</p>}
                 </div>
                 <div>
                     <label className="block font-medium text-gray-300">Message</label>
-                    <textarea {...register('message')} placeholder='....' className="w-full h-[12vh] border-2 p-1 bg-white  rounded"></textarea>
+                    <textarea {...register('message')} placeholder='....' className="w-full h-[12vh] border-2 p-1 bg-white/90  rounded-xl"></textarea>
                     {errors.message && <p className="text-red-900/80 text-xs">{errors.message.message}</p>}
                 </div>
-                <button disabled={isSubmitting} className="bg-black text-white px-4 py-2 rounded">
+                <button disabled={isSubmitting} className="border-2 w-full p-2 text-white rounded-lg bg-gradient-to-r from-[#ff8c00] to-[#8a2be2] opacity-80 uppercase font-extrabold hover:opacity-100">
                     {isSubmitting ? 'Sending...' : 'Send Message'}
                 </button>
             </form>
