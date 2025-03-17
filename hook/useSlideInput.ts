1947ea31bfe3a6d8d7f599e003134846e057757c
June Trinh <thanchitr@outlook.com>
2025-03-17 21:09:18 +1100

    feat(touch-slider)

--- hook/useSlideInput.ts
+++ hook/useSlideInput.ts
@@ -27,14 +27,32 @@ const useSlideInput = (
       }
       setDownX(null);
     };
+    const tdownLog = (e: TouchEvent) => {
+      setDownX(e.touches[0].clientX);
+    };
+
+    const tupLog = (e: TouchEvent) => {
+      if (downX === null) return;
+
+      if (downX - e.changedTouches[0].clientX > 150) {
+        toRightHandler();
+      }
+      if (downX - e.changedTouches[0].clientX < -150) {
+        toLeftHandler();
+      }
+      setDownX(null);
+    };
 
     const element = container.current;
     element.addEventListener("mousedown", downLog);
     document.addEventListener("mouseup", upLog); // `mouseup` should still be on `document`
-
+    element.addEventListener("touchstart", tdownLog);
+    document.addEventListener("touchend", tupLog); // `mouseup` should still be on `document`
     return () => {
       element.removeEventListener("mousedown", downLog);
       document.removeEventListener("mouseup", upLog);
+      element.removeEventListener("touchstart", tdownLog);
+      document.removeEventListener("touchend", tupLog);
     };
   }, [downX, container, toRightHandler, toLeftHandler]);
 
